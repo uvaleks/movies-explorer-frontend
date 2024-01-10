426a6bb8ad12a3a8927caf0172e4cd950a047d81
uvaleks <uvaleks@protonmail.com>
2024-01-10 23:05:03 +0300

    feat: create responsive footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,6 @@ export default function Header() {
         <header className="header">
         <img className="header__logo" src={logoPath} alt="Movies Exlplorer Logo"/>
         <Navigation/>
-        {/* <div className={`header__menu ${isMobileMenuOpened ? 'header__menu_opened' : ''}`}>
-          {loggedIn && <a className="header__menu-email">{userEmail}</a>}
-          <Link onClick={handleSignupMenuClick} className="header__menu-button">{loggedIn ? "Выйти" : (isUserOnSignupScreeen ? 'Войти' : 'Зарегистрироваться')}</Link>
-        </div>
-        <Link onClick={handleMenuClick} className={`header__menu-burger ${isMobileMenuOpened ? 'header__menu-burger_icon_close' : 'header__menu-burger_icon_burger'}`}></Link> */}
         </header>     
   );
 }
