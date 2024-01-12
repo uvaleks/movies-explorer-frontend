911f7eed8c3b512fe6d8e8595e252f626005f519
uvaleks <uvaleks@protonmail.com>
2024-01-12 23:44:51 +0300

    feat: create responsive techs section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,9 @@ import Header from '../Header/Header';
 import Promo from './Promo/Promo';
 import NavTab from './NavTab/NavTab';
 import Footer from '../Footer/Footer';
-import AboutProject from './AboutProject/AboutProject'
-import AboutMe from './AboutMe/AboutMe'
+import AboutProject from './AboutProject/AboutProject';
+import Techs from './Techs/Techs';
+import AboutMe from './AboutMe/AboutMe';
 import './Main.css';
 
 export default function Main() {
@@ -13,6 +14,7 @@ export default function Main() {
           <Promo/>
           <NavTab/>
           <AboutProject/>
+          <Techs/>
           <AboutMe/>
           <Footer/>
         </>     
